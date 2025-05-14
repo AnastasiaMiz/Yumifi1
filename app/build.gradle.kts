349f90683780d1ba778aa702edfac9898fe1fd31
Shiryaev Nikolay <deendeeps@list.ru>
2025-05-14 15:20:17 +0300

    Подключение зависимостей

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt)
+    kotlin("kapt")
 }
 
 android {
@@ -56,4 +58,25 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    implementation(libs.hilt.navigation.compose)
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // Navigation
+    implementation(libs.navigation)
+
+    // Coil
+    implementation(libs.coil)
+
+    // Room
+    implementation(libs.room.runtime)
+    implementation(libs.room.compiler)
+    kapt(libs.room.compiler)
+    implementation(libs.room.ktx)
+}
+
+kapt {
+    correctErrorTypes = true
 }
