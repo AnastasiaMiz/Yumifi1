96d375742a3a5e5b01e467ddbb5945fe43db699f
Shiryaev Nikolay <deendeeps@list.ru>
2025-05-14 18:02:03 +0300

    Удаляется иконка на сплэш экране

--- app/src/main/java/com/example/yumifi1/features/splash/SplashView.kt
+++ app/src/main/java/com/example/yumifi1/features/splash/SplashView.kt
@@ -16,12 +16,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.yumifi1.R
 import com.example.yumifi1.features.splash.event.SplashEvent
 import com.example.yumifi1.navigation.Screens
@@ -41,6 +44,10 @@ fun SplashView(
             navController.navigate(screen.route)
         }
     }
+    val context = LocalContext.current
+    val appIcon = context.packageManager.getApplicationIcon(
+        context.applicationInfo
+    )
 
     Scaffold { contentPadding ->
         Column(
@@ -53,14 +60,6 @@ fun SplashView(
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Icon(
-                    painter = painterResource(id = R.mipmap.ic_launcher_round),
-                    contentDescription = null,
-                    tint = MaterialTheme.colorScheme.secondary,
-                    modifier = Modifier
-                        .size(100.dp)
-                )
-                Spacer(modifier = Modifier.height(16.dp))
                 Text(
                     text = stringResource(id = R.string.app_name),
                     fontSize = 20.sp,
